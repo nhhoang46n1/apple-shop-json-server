b0da85b65be6e589edb5c6a6b1f56d95c5c3259a
Nguyen Huy Hoang <nhhoang46n1@gmail.com>
2022-06-21 20:54:20 +0700

    upload

---
+++ db.js
@@ -0,0 +1,15 @@
+const iphone = require("./data/iphone.json");
+const ipad = require("./data/ipad.json");
+const mac = require("./data/mac.json");
+const watch = require("./data/watch.json");
+const airpod = require("./data/airpod.json");
+const accessories = require("./data/accessories.json");
+
+module.exports = {
+  iphone: iphone,
+  ipad: ipad,
+  mac: mac,
+  watch: watch,
+  airpods: airpod,
+  accessories: accessories,
+};
---
+++ main.js
@@ -0,0 +1,32 @@
+const jsonServer = require("json-server");
+
+const data = require("./db.js");
+const server = jsonServer.create();
+const router = jsonServer.router(data);
+const middlewares = jsonServer.defaults();
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares);
+
+// Add custom routes before JSON Server router
+server.get("/echo", (req, res) => {
+  res.jsonp(req.query);
+});
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser);
+server.use((req, res, next) => {
+  if (req.method === "POST") {
+    req.body.createdAt = Date.now();
+  }
+  // Continue to JSON Server router
+  next();
+});
+
+// Use default router
+server.use("/api", router);
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log("JSON Server is running");
+});
